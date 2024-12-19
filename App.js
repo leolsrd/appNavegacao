968404d884f431f4e26bd63c455bdc7a24d3fd4f
Léo Lourenço <leo.lsrd@gmail.com>
2024-12-18 23:55:51 -0300

    feat: Começando o código sobre navegação com tabs

--- App.js
+++ App.js
@@ -10,25 +10,5 @@ import Contato from './src/pages/Contato';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: 'Tela Inicial',
-            headerStyle: {
-              backgroundColor: '#121212',
-            },
-            headerTintColor: '#FFF',
-            // * Removendo a barra superior do app com o nome Home
-            // headerShown: false,
-          }}
-        />
-        <Stack.Screen name="Sobre" component={Sobre} />
-        <Stack.Screen name="Contato" component={Contato} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <NavigationContainer></NavigationContainer>;
 }
--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-
-import {useNavigation} from '@react-navigation/native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function Contato() {
   return (
