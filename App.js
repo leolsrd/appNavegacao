2ea705969f548f42b68537a945da3ab2237f2bb9
Léo Lourenço <leo.lsrd@gmail.com>
2024-12-20 20:04:59 -0300

    feat: Finalizado a aula sobre o modo de navegação tab

--- App.js
+++ App.js
@@ -7,17 +7,59 @@ import Home from './src/pages/Home';
 import Sobre from './src/pages/Sobre';
 import Contato from './src/pages/Contato';
 
+import Feather from 'react-native-vector-icons/Feather';
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+      <Tab.Navigator
+        screenOptions={{
+          // ? Removendo o Header do topo de todas as tabs
+          headerShown: false,
+          // ? Quando abrir um campo de input e o teclado for aberto o tab
+          // ? fica escondido
+          tabBarHideOnKeyboard: true,
+          // ? Removendo o nome do icone
+          tabBarShowLabel: false,
+          // ? Mudando a cor do background da tab
+          tabBarStyle: {
+            backgroundColor: '#000000',
+          },
+          // ? Mudando a cor da tab ativa
+          tabBarActiveTintColor: '#ffffff',
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            // tabBarLabel: 'Inicio',
+            tabBarIcon: ({color, size}) => {
+              return <Feather name="home" color={color} size={size} />;
+            },
+          }}
+        />
 
-        <Tab.Screen name="Sobre" component={Sobre} />
+        <Tab.Screen
+          name="Sobre"
+          component={Sobre}
+          options={{
+            tabBarIcon: ({color, size}) => {
+              return <Feather name="file-text" color={color} size={size} />;
+            },
+          }}
+        />
 
-        <Tab.Screen name="Contato" component={Contato} />
+        <Tab.Screen
+          name="Contato"
+          component={Contato}
+          options={{
+            tabBarIcon: ({color, size}) => {
+              return <Feather name="phone-call" size={size} color={color} />;
+            },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
